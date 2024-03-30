d64c1fa8e174d04b8b52c5bd3ac238c56e80c61a
Martin <martinvali12@gmail.com>
2024-03-30 18:17:16 +0200

    Add project files.

---
+++ AgileWorks/Models/Ticket.cs
@@ -0,0 +1,8 @@
+﻿namespace AgileWorks.Models {
+    public class Ticket {
+        
+        public string Description { get;set; }
+        public DateTime DateCreated { get;set; }
+        public DateTime DueDate { get;set; }
+    }
+}
