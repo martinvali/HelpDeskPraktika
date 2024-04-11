50e9f2591b7abb26a66e583d6b1b106acec4381f
Martin <martinvali12@gmail.com>
2024-04-11 10:42:01 +0300

    Remove unnecessary files and folders

--- AgileWorks/Controllers/TicketsController.cs
+++ AgileWorks/Controllers/TicketsController.cs
@@ -28,22 +28,6 @@ namespace AgileWorks.Controllers
             });
             return View(tickets);
         }
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var ticket = await _context.Ticket
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (ticket == null)
-            {
-                return NotFound();
-            }
-            ViewData["Title"] = "Details";
-            return View(ticket);
-        }
         public IActionResult Create()
         {
             ViewData["Title"] = "Create";
@@ -62,72 +46,10 @@ namespace AgileWorks.Controllers
             }
             return View(ticket);
         }
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var ticket = await _context.Ticket.FindAsync(id);
-            if (ticket == null)
-            {
-                return NotFound();
-            }
-            ViewData["Title"] = "Edit";
-            return View(ticket);
-        }
-
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Description,DueDate")] Ticket ticket)
-        {
-            if (id != ticket.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(ticket);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TicketExists(ticket.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(ticket);
-        }
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var ticket = await _context.Ticket
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (ticket == null)
-            {
-                return NotFound();
-            }
-            ViewData["Title"] = "Delete";
-            return View(ticket);
-        }
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> Delete(int id)
         {
             var ticket = await _context.Ticket.FindAsync(id);
             if (ticket != null)
